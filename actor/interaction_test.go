878d31911ef1fda02473c40e8f2b05b79f187af0
huangshufan <charleshuang@lilith.com>
2022-08-17 18:55:23 +0800

    1

---
+++ actor/interaction_test.go
@@ -0,0 +1,271 @@
+package actor
+
+import (
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+)
+
+type DummyMessage struct{}
+type BlackHoleActor struct{}
+
+var testTimeout = 1 * time.Second
+
+func (state *BlackHoleActor) Receive(context Context) {}
+
+func NewBlackHoleActor() Actor {
+	return &BlackHoleActor{}
+}
+
+func TestSpawnProducesActorRef(t *testing.T) {
+	actor := Spawn(Props(NewBlackHoleActor))
+	defer actor.Stop()
+	assert.NotNil(t, actor)
+}
+
+type EchoMessage struct{ Sender *PID }
+
+type EchoReplyMessage struct{}
+
+type EchoActor struct{}
+
+func NewEchoActor() Actor {
+	return &EchoActor{}
+}
+
+func (*EchoActor) Receive(context Context) {
+	switch msg := context.Message().(type) {
+	case EchoMessage:
+		msg.Sender.SendMsg(EchoReplyMessage{})
+	}
+}
+
+func TestActorCanReplyToMessage(t *testing.T) {
+	responsePID, result := RequestResponsePID()
+	actor := Spawn(Props(NewEchoActor))
+	defer actor.Stop()
+	actor.SendMsg(EchoMessage{Sender: responsePID})
+	if _, err := result.ResultOrTimeout(testTimeout); err != nil {
+		assert.Fail(t, "timed out")
+		return
+	}
+}
+
+type BecomeMessage struct{}
+
+type EchoBecomeActor struct{}
+
+func NewEchoBecomeActor() Actor {
+	return &EchoBecomeActor{}
+}
+
+func (state *EchoBecomeActor) Receive(context Context) {
+	switch context.Message().(type) {
+	case BecomeMessage:
+		context.Become(state.Other)
+	}
+}
+
+func (EchoBecomeActor) Other(context Context) {
+	switch msg := context.Message().(type) {
+	case EchoMessage:
+		msg.Sender.SendMsg(EchoReplyMessage{})
+	}
+}
+
+func TestActorCanBecome(t *testing.T) {
+	responsePID, result := RequestResponsePID()
+	actor := Spawn(Props(NewEchoActor))
+	defer actor.Stop()
+	actor.SendMsg(BecomeMessage{})
+	actor.SendMsg(EchoMessage{Sender: responsePID})
+	if _, err := result.ResultOrTimeout(testTimeout); err != nil {
+		assert.Fail(t, "timed out")
+		return
+	}
+}
+
+type UnbecomeMessage struct{}
+
+type EchoUnbecomeActor struct{}
+
+func NewEchoUnbecomeActor() Actor {
+	return &EchoBecomeActor{}
+}
+
+func (state *EchoUnbecomeActor) Receive(context Context) {
+	switch msg := context.Message().(type) {
+	case BecomeMessage:
+		context.BecomeStacked(state.Other)
+	case EchoMessage:
+		msg.Sender.SendMsg(EchoReplyMessage{})
+	}
+}
+
+func (*EchoUnbecomeActor) Other(context Context) {
+	switch context.Message().(type) {
+	case UnbecomeMessage:
+		context.UnBecomeStacked()
+	}
+}
+
+func TestActorCanUnbecome(t *testing.T) {
+	responsePID, result := RequestResponsePID()
+	actor := Spawn(Props(NewEchoActor))
+	defer actor.Stop()
+	actor.SendMsg(BecomeMessage{})
+	actor.SendMsg(UnbecomeMessage{})
+	actor.SendMsg(EchoMessage{Sender: responsePID})
+	if _, err := result.ResultOrTimeout(testTimeout); err != nil {
+		assert.Fail(t, "timed out")
+		return
+	}
+}
+
+type EchoOnStartActor struct{ replyTo *PID }
+
+func (state *EchoOnStartActor) Receive(context Context) {
+	switch context.Message().(States) {
+	case States_Started:
+		state.replyTo.SendMsg(EchoReplyMessage{})
+	}
+}
+
+func NewEchoOnStartActor(replyTo *PID) func() Actor {
+	return func() Actor {
+		return &EchoOnStartActor{replyTo: replyTo}
+	}
+}
+
+// func TestActorCanReplyOnStarting(t *testing.T) {
+// 	responsePID,result := FuturePID()
+// 	actor := Spawn(Props(NewEchoOnStartActor(responsePID)))
+// 	defer actor.Stop()
+// 	if _, err := result.ResultOrTimeout(testTimeout); err != nil {
+// 		assert.Fail(t, "timed out")
+// 		return
+// 	}
+// }
+
+type EchoOnStoppingActor struct{ replyTo *PID }
+
+func (state *EchoOnStoppingActor) Receive(context Context) {
+	switch context.Message().(States) {
+	case States_Stopping:
+		state.replyTo.SendMsg(EchoReplyMessage{})
+	}
+}
+
+func NewEchoOnStoppingActor(replyTo *PID) func() Actor {
+	return func() Actor {
+		return &EchoOnStoppingActor{replyTo: replyTo}
+	}
+}
+
+func TestActorCanReplyOnStopping(t *testing.T) {
+	responsePID, result := RequestResponsePID()
+	actor := Spawn(Props(NewEchoOnStoppingActor(responsePID)))
+	actor.Stop()
+	if _, err := result.ResultOrTimeout(testTimeout); err != nil {
+		assert.Fail(t, "timed out")
+		return
+	}
+}
+
+type CreateChildMessage struct{}
+type GetChildCountMessage struct{ ReplyTo *PID }
+type GetChildCountReplyMessage struct{ ChildCount int }
+type CreateChildActor struct{}
+
+func (*CreateChildActor) Receive(context Context) {
+	switch msg := context.Message().(type) {
+	case CreateChildMessage:
+		context.Spawn(Props(NewBlackHoleActor))
+	case GetChildCountMessage:
+		reply := GetChildCountReplyMessage{ChildCount: len(context.Children())}
+		msg.ReplyTo.SendMsg(reply)
+	}
+}
+
+func NewCreateChildActor() Actor {
+	return &CreateChildActor{}
+}
+
+func TestActorCanCreateChildren(t *testing.T) {
+	responsePID, result := RequestResponsePID()
+	actor := Spawn(Props(NewCreateChildActor))
+	defer actor.Stop()
+	expected := 10
+	for i := 0; i < expected; i++ {
+		actor.SendMsg(CreateChildMessage{})
+	}
+	actor.SendMsg(GetChildCountMessage{ReplyTo: responsePID})
+	response, err := result.ResultOrTimeout(testTimeout)
+	if err != nil {
+		assert.Fail(t, "timed out")
+		return
+	}
+	assert.Equal(t, expected, response.(GetChildCountReplyMessage).ChildCount)
+}
+
+type CreateChildThenStopActor struct {
+	replyTo *PID
+}
+
+type GetChildCountMessage2 struct {
+	ReplyDirectly  *PID
+	ReplyAfterStop *PID
+}
+
+func (state *CreateChildThenStopActor) Receive(context Context) {
+	msg, ok := context.Message().(States)
+	if !ok {
+		switch msg := context.Message().(type) {
+		case CreateChildMessage:
+			context.Spawn(Props(NewBlackHoleActor))
+		case GetChildCountMessage2:
+			msg.ReplyDirectly.SendMsg(true)
+			state.replyTo = msg.ReplyAfterStop
+		}
+	} else {
+		if msg == States_Stopped {
+			reply := GetChildCountReplyMessage{ChildCount: len(context.Children())}
+			state.replyTo.SendMsg(reply)
+		}
+	}
+}
+
+func NewCreateChildThenStopActor() Actor {
+	return &CreateChildThenStopActor{}
+}
+
+func TestActorCanStopChildren(t *testing.T) {
+	responsePID, result := RequestResponsePID()
+	responsePID2, result2 := RequestResponsePID()
+	actor := Spawn(Props(NewCreateChildThenStopActor))
+	count := 10
+	for i := 0; i < count; i++ {
+		actor.SendMsg(CreateChildMessage{})
+	}
+	actor.SendMsg(GetChildCountMessage2{ReplyDirectly: responsePID, ReplyAfterStop: responsePID2})
+
+	//wait for the actor to reply to the first responsePID
+	_, err := result.ResultOrTimeout(testTimeout)
+	if err != nil {
+		assert.Fail(t, "timed out")
+		return
+	}
+
+	//then send a stop command
+	actor.Stop()
+
+	//wait for the actor to stop and get the result from the stopped handler
+	response, err := result2.ResultOrTimeout(testTimeout)
+	if err != nil {
+		assert.Fail(t, "timed out")
+		return
+	}
+	//we should have 0 children when the actor is stopped
+	assert.Equal(t, 0, response.(GetChildCountReplyMessage).ChildCount)
+}
--- go.mod
+++ go.mod
@@ -5,7 +5,9 @@ go 1.17
 require github.com/streadway/amqp v1.0.0
 
 require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/sony/sonyflake v1.0.0 // indirect
 	golang.org/x/net v0.0.0-20201021035429-f5854403a974 // indirect
 	golang.org/x/sys v0.0.0-20210119212857-b64e53b001e4 // indirect
@@ -13,9 +15,11 @@ require (
 	google.golang.org/genproto v0.0.0-20200526211855-cb27e3aa2013 // indirect
 	google.golang.org/grpc v1.48.0 // indirect
 	google.golang.org/protobuf v1.27.1 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
 
 require (
 	github.com/asynkron/goconsole v0.0.0-20160504192649-bfa12eebf716
 	github.com/gogo/protobuf v1.3.2 // indirect
+	github.com/stretchr/testify v1.8.0
 )

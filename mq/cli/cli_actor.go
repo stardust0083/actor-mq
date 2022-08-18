e38f17f2d12d0a1685ed9b2b9d04246c9bb030d7
huangshufan <charleshuang@lilith.com>
2022-08-18 19:02:08 +0800

    tmop

--- mq/cli/cli_actor.go
+++ mq/cli/cli_actor.go
@@ -2,16 +2,30 @@ package cli
 
 import (
 	"actor-mq/actor"
+	"actor-mq/mq/pb"
 	"actor-mq/remote"
 	"fmt"
 )
+
+type routerMgr struct {
+	RouterList []*actor.PID
+}
+
+func (r *routerMgr) Receive(context actor.Context) {
+	switch msg := context.Message().(type) {
+	case *pb.SyncRouterMsg:
+		r.RouterList = msg.Router
+		fmt.Println(r.RouterList)
+	}
+}
 
 type CliUser struct {
 }
 
 func StartClient(host string, port string) {
 	remote.StartServer(fmt.Sprintf("%s:%s", host, port))
-
+	pidrm := actor.SpawnTemplate(&routerMgr{RouterList: make([]*actor.PID, 0)})
+	actor.PIDMgr.Register("RouterManager", pidrm)
 }
 
 func NewUser() *actor.PID {
